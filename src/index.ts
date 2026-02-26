7698655e40b165733decc4fe4baecb8ab9e33c54
Paulo Marcelo <marcelomp97447@gmail.com>
2026-02-26 20:36:28 -0300

    fix: adjusting line of dotenv.config()

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import "reflect-metadata";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config();
+
 import { AppDataSource } from "./config/data-source";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("API de Autenticação funcionando 🚀");
