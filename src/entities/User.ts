048ea53c3e307f02e8cc8542afde4edcecae23cc
Paulo Marcelo <marcelomp97447@gmail.com>
2026-02-26 19:43:13 -0300

    feat: adding user entity

---
+++ src/entities/User.ts
@@ -0,0 +1,30 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from "typeorm";
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  isVerified: boolean;
+
+  @Column({ nullable: true })
+  provider: string;
+
+  @Column({ nullable: true })
+  providerId: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
