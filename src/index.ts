d21a7a377f7a61682a68947d81e586a6f571b2e8
Paulo Marcelo <marcelomp97447@gmail.com>
2026-02-26 19:51:01 -0300

    chore: adding database config in dockecompose

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import express from "express";
+import cors from "cors";
+import { AppDataSource } from "./config/data-source";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API de Autenticação funcionando 🚀");
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Banco conectado");
+    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
+  })
+  .catch((err) => console.error("Erro ao conectar DB:", err));
